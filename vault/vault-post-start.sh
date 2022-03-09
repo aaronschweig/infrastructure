1618bbd7d64f981b840bcf7e37ce2f523ed77f57
Aaron Schweig <aaron.schweig@sap.com>
2022-03-09 15:25:06 +0100

    cleaning up of vault application

---
+++ vault/vault-post-start.sh
@@ -0,0 +1,11 @@
+# grant read access to "secret" kv store
+printf 'path "secret/*"{\ncapabilities=["read"]\n}' | vault policy write argocd-policy -
+
+# enable k8s authentication
+vault auth enable kubernetes
+
+# configure k8s authentication
+vault write auth/kubernetes/config kubernetes_host=https://kubernetes.default.svc:443 disable_iss_validation=true
+
+# write role for default service account to access vault from every namespace
+vault write auth/kubernetes/role/argocd bound_service_account_names=default bound_service_account_namespaces="*" policies="argocd-policy"
