5226e445df6cd5e37b4220e8e75207fb59353f06
Aaron Schweig <aaron.schweig@gmail.com>
2022-03-13 12:15:01 +0100

    enables oidc login for vault

--- vault/vault-post-start.sh
+++ vault/vault-post-start.sh
@@ -9,3 +9,13 @@ vault write auth/kubernetes/config kubernetes_host=https://kubernetes.default.sv
 
 # write role for default service account to access vault from every namespace
 vault write auth/kubernetes/role/argocd bound_service_account_names=default bound_service_account_namespaces="*" policies="argocd-policy"
+
+printf 'path "secret/*" {\ncapabilities=["create", "read", "update", "delete", "list"]\n}' | vault policy write oidc-policy -
+
+vault auth enable oidc
+
+vault write auth/oidc/config oidc_client_id="vault-oidc" oidc_client_secret="$VAULT_CLIENT_SECRET" oidc_discovery_url="https://argocd.aaronschweig.dev/api/dex" default_role="oidc-role"
+
+vault write auth/oidc/role/oidc-role user_claim="sub" \
+    allowed_redirect_uris="http://localhost:8250/oidc/callback,https://vault.aaronschweig.dev/ui/vault/auth/oidc/oidc/callback" \
+    policies="oidc-policy"
